d6157f849ebe9133d90f74521cc9529b7dee997a
Pnt-C <6631501071@lamduan.mfu.ac.th>
2025-09-11 15:34:52 +0700

    ✨ Replace SingleChildDemo with MultiChildDemo widget for enhanced layout functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/week05/single_child_demo.dart';
+import 'package:flutter_application_1/week05/multi_child_demo.dart';
 
 void main() {
   runApp(
-    MaterialApp(home: SingleChildDemo(), debugShowCheckedModeBanner: false),
+    MaterialApp(home: MultiChildDemo(), debugShowCheckedModeBanner: false),
   );
 }
