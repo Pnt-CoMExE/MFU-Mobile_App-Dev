a85b628ec97f2db1b9712ef517c6704e4b29140b
Pnt-C <6631501071@lamduan.mfu.ac.th>
2025-09-11 14:53:54 +0700

    ✨ Update .gitignore to include platform-specific directories and add SingleChildDemo widget for improved UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/week04/assignment2.dart';
+import 'package:flutter_application_1/week05/single_child_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: Assignment2(), debugShowCheckedModeBanner: false));
+  runApp(
+    MaterialApp(home: SingleChildDemo(), debugShowCheckedModeBanner: false),
+  );
 }
