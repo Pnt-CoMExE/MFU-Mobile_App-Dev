cc9eead8f84bb77160285799cd1bfb85a0713c01
Pnt-CoMExE <comza962@gmail.com>
2025-09-21 17:33:58 +0700

    Fix image asset path in ImageDemo widget and update pubspec.yaml formatting

--- lib/week06/image_demo.dart
+++ lib/week06/image_demo.dart
@@ -11,7 +11,7 @@ class ImageDemo extends StatelessWidget {
           color: Colors.yellow,
           width: 200,
           height: 200,
-          child: Image.asset('assets/images/sky.jpeg', fit: BoxFit.cover),
+          child: Image.asset('assets/images/sky.jpg', fit: BoxFit.cover),
           // child: Image.network(
           //   'https://i.pinimg.com/originals/2e/c6/b5/2ec6b5e14fe0cba0cb0aa5d2caeeccc6.jpg',
           //   fit: BoxFit.cover,
