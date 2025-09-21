771df482ab4041e6305e8b574f10e8bacb454ae6
Pnt-CoMExE <comza962@gmail.com>
2025-09-21 19:07:32 +0700

    ✨ Add BakingApp, LoginDynamic, and TimeDemo widgets with UI and functionality

---
+++ lib/week06/time_demo.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class TimeDemo extends StatefulWidget {
+  const TimeDemo({super.key});
+
+  @override
+  State<TimeDemo> createState() => _TimeDemoState();
+}
+
+class _TimeDemoState extends State<TimeDemo> {
+  // String message = 'Start';
+  int count = 5;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // create a countdown timer
+    // Timer(Duration(seconds: 3), () {
+    //   setState(() {
+    //     message = 'Stop';
+    //   });
+    // });
+
+    // Future.delayed(Duration(seconds: 3), () {
+    //   setState(() {
+    //     message = 'Stop';
+    //   });
+    // });
+
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        count--;
+        if (count == 0) {
+          timer.cancel();
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Text(count.toString(), style: TextStyle(fontSize: 30)),
+        ),
+      ),
+    );
+  }
+}
